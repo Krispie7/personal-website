41538e1e073e2d51e2cacd3f30788c2c3bf03b19
Kristian <kr.mendoza.sh@gmail.com>
2023-04-14 19:25:28 -0400

    added stopping on hover for earth

--- script.js
+++ script.js
@@ -10,5 +10,49 @@ function ooga(){
     content.style.display="block"
     fadeInBackground.style.opacity="0"
     fadeInBackground.style.height="400vh"
+    completFadeOut()
     return
 }
+
+async function completFadeOut(){
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    fadeInBackground.style.display="none"
+}
+
+const earth=document.querySelector("#earth .planet");
+const mars=document.querySelector("#mars .planet");
+const jupiter=document.querySelector("#jupiter .planet");
+const uranus=document.querySelector("#uranus .planet");
+const sun=document.getElementById("sun");
+
+console.log(earth)
+sun.addEventListener("mouseover", sayhi);
+earth.addEventListener("mouseover", hoverEarth);
+earth.addEventListener("mouseout", unHoverEarth);
+
+function sayhi(){
+    console.log("ooga");
+}
+
+function hoverEarth(){
+    const orbit=document.getElementById("earth");
+    const container=document.querySelector("#earth .planet-container")
+    const planet=earth
+    orbit.style.animationPlayState="paused"
+    container.style.animationPlayState="paused"
+    planet.style.animationPlayState="paused"
+    console.log("pause")
+    return
+}
+
+function unHoverEarth(){
+    const orbit=document.getElementById("earth");
+    const container=document.querySelector("#earth .planet-container")
+    const planet=earth
+    orbit.style.animationPlayState="running"
+    container.style.animationPlayState="running"
+    planet.style.animationPlayState="running"
+    console.log("resume")
+    return
+}
+
